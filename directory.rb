fdd1a18678577ebef80fde1876d3fbea0e4919e4
Karen Roberts <lanxuin@gmail.com>
2018-10-23 10:09:28 +0000

    Step 8 Exercise 1 - added a number before each student name

--- directory.rb
+++ directory.rb
@@ -1,16 +1,17 @@
 def input_students
-  puts  "Please enter the names of the students"
+  puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
+
   while !name.empty? do
-    # add the student hash tot he array
+    # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Please enter the name of the student"
     name = gets.chomp
   end
   # return the array of students
@@ -23,9 +24,9 @@ def print_header
 end 
 
 def print_dir(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+	 students.each_with_index do |student, index|
+	   puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+	 end
 end
 
 def print_footer(names)
